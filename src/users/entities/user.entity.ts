8b62abad4ac36057a0552dda3f315e43e16a8886
Patrick Girardi <dev.patrickgirardi@gmail.com>
2025-07-30 19:30:12 -0300

    feat(users): add user entity class to define user model structure

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
